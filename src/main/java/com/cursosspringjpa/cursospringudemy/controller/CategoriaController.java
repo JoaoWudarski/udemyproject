0e5729edb0e6b62c35dc62efb536277204d83a8c
joaowudarski <joaowudarski@gmail.com>
2020-11-20 16:30:01 -0300

    Test new class

--- src/main/java/com/cursosspringjpa/cursospringudemy/controller/CategoriaController.java
+++ src/main/java/com/cursosspringjpa/cursospringudemy/controller/CategoriaController.java
@@ -1,5 +1,9 @@
 package com.cursosspringjpa.cursospringudemy.controller;
 
+import java.util.ArrayList;
+
+import com.cursosspringjpa.cursospringudemy.model.Categoria;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaController {
     
     @GetMapping()
-    public String listar(){
-        return "Rest is alright";
+    public ArrayList<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(1, "Escritório");
+
+        ArrayList<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
