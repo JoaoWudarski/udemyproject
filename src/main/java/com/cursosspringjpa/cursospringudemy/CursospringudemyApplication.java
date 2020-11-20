6e6da2c26f4f07a7b40080f8d146d476ffaaffdd
joaowudarski <joaowudarski@gmail.com>
2020-11-20 19:02:48 -0300

    Instance, CommandLine

--- src/main/java/com/cursosspringjpa/cursospringudemy/CursospringudemyApplication.java
+++ src/main/java/com/cursosspringjpa/cursospringudemy/CursospringudemyApplication.java
@@ -1,13 +1,31 @@
 package com.cursosspringjpa.cursospringudemy;
 
+import java.util.Arrays;
+
+import com.cursosspringjpa.cursospringudemy.model.Categoria;
+import com.cursosspringjpa.cursospringudemy.repository.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursospringudemyApplication {
+public class CursospringudemyApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository rep;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringudemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		rep.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/cursosspringjpa/cursospringudemy/controller/CategoriaController.java
+++ src/main/java/com/cursosspringjpa/cursospringudemy/controller/CategoriaController.java
@@ -25,4 +25,6 @@ public class CategoriaController {
         
         return ResponseEntity.ok(cat);
     }
+
+    
 }
