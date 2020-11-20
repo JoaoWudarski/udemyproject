31eb3d2d81d7934e2c91e7224a92325d3302cf69
joaowudarski <joaowudarski@gmail.com>
2020-11-20 15:41:43 -0300

    Projeto criado

---
+++ src/test/java/com/cursosspringjpa/cursospringudemy/CursospringudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosspringjpa.cursospringudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
