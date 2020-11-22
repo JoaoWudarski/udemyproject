3fe0135aa6b1814bd45babd52df76a21e2ef9cfe
joaowudarski <joaowudarski@gmail.com>
2020-11-22 19:07:31 -0300

    Endpoint/clientes/id -Available

---
+++ src/main/java/com/cursosspringjpa/cursospringudemy/controller/ClienteController.java
@@ -0,0 +1,30 @@
+package com.cursosspringjpa.cursospringudemy.controller;
+
+
+import com.cursosspringjpa.cursospringudemy.model.Cliente;
+import com.cursosspringjpa.cursospringudemy.service.ClienteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService srvc;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listar(@PathVariable Integer id){
+
+        Cliente cat = srvc.buscar(id);
+        
+        return ResponseEntity.ok(cat);
+    }
+
+    
+}
---
+++ src/main/java/com/cursosspringjpa/cursospringudemy/service/ClienteService.java
@@ -0,0 +1,26 @@
+package com.cursosspringjpa.cursospringudemy.service;
+
+import java.util.Optional;
+
+import com.cursosspringjpa.cursospringudemy.model.Cliente;
+import com.cursosspringjpa.cursospringudemy.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class ClienteService {
+    
+    @Autowired
+    private ClienteRepository rep;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> op = rep.findById(id);
+        
+        return op.orElseThrow( () -> new ResponseStatusException
+                             (HttpStatus.NOT_FOUND, "Cliente não encontrada, ID = "
+                              + id + " Tipo: " + Cliente.class.getName()));
+    } 
+}
